cb85ffa0d1d470008eee12ed3c986cf6081daf0e
Rubensrvsc <Rubensspfc100@gmail.com>
2019-05-24 07:11:35 -0300

    ok

--- appCliente/urls.py
+++ appCliente/urls.py
@@ -8,7 +8,7 @@ urlpatterns =[
     #path('login_cliente/', v.LoginView.as_view(template_name='login_cliente.html'), name = 'login'), 
     #path('logout_usuario/', v.LogoutView.as_view(template_name='login_cliente.html'), name="logout"),
     path('registrar_cliente/',registrar_cliente,name='registrar_cliente'),
-    path('obter_estacionamento/',obter_estacionamentos,name='obter_estacionamentos'),
-    path('index_cliente/',IndexView.as_view(),name='index_cliente'),
+    path('obter_estacionamentos/',obter_estacionamentos,name='obter_estacionamentos'),
+    #path('index_cliente/',IndexView.as_view(),name='index_cliente'),
     #path('login_cliente/', include('rest_framework.urls')),
 ]
